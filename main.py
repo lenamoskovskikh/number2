971591e950e24513032c9f9eee896713199d4ee0
lena moskovskikh <lena.moskovskikh@gmail.com>
2022-11-27 03:16:16 +0300

    новый класс - дизайн программы

--- main.py
+++ main.py
@@ -1,15 +1,66 @@
 from PyQt5.QtWidgets import QWidget, QApplication, QPushButton, QMainWindow
 from PyQt5 import uic
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPainter, QColor
 import sys
 from random import randint
 
+from PyQt5 import QtCore, QtGui, QtWidgets
 
-class Example(QMainWindow):
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(1038, 751)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(380, 570, 251, 61))
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1038, 26))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "нарисовать"))
+
+
+class Example(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('ui.ui', self)
+        self.setupUi(self)
+        self.flag = False
+        self.pushButton.clicked.connect(self.run)
 
+    def paintEvent(self, event):
+        if self.flag:
+            self.qp = QPainter()
+            self.qp.begin(self)
+            self.paint()
+            self.qp.end()
 
+    def run(self):
+        self.flag = True
+        self.repaint()
+
+    def paint(self):
+        x = randint(70, 300)
+        y = randint(70, 300)
+        z = randint(70, 300)
+        self.qp.setBrush(QColor('Yellow'))
+        self.qp.drawEllipse(720, 50, x, x)
+        self.qp.drawEllipse(410, 250, y, y)
+        self.qp.drawEllipse(100, 100, z, z)
 
 
 if __name__ == '__main__':
